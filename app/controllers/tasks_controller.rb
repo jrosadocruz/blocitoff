18e4fc07780301739a9164f043326e0fd9891a4d
Jose Rosado <joserosado@esd.com.do>
2015-07-01 11:53:30 -0400

    Delete and create with ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,13 +10,32 @@ class TasksController < ApplicationController
   def create
     @user = current_user
     @task = @user.tasks.build(task_params)
+    @new_task = @user.tasks.build(task_params)
     if @task.save
       flash[:notice] = "Task was created."
-      redirect_to authenticated_root_path
     else
       flash[:error] = "There was an error creating the task. Please try again."
-      redirect_to authenticated_root_path
     end
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @task = @user.tasks.find(params[:id])
+    if @task.destroy
+      flash[:notice] = "Task was completed."
+    else
+      flash[:error] = "There was an error completing the task. Please try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+   end
+
   end
 
   private
