ca9c90e028fee2dd009d8cda45a2fc537f20eb9a
Jose Rosado <joserosado@esd.com.do>
2015-07-02 11:11:17 -0400

    Added days left.

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,8 @@ class Task < ActiveRecord::Base
   validates :title , presence: true
   validates :user  , presence: true
 
+  def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
+
 end
